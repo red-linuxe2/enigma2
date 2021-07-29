133f9b9834c8ded26e8dc947bf0754a606bccc9a
red-linuxe2 <88103659+red-linuxe2@users.noreply.github.com>
2021-07-29 19:24:41 +0100

    Update ewidget.cpp

--- lib/gui/ewidget.cpp
+++ lib/gui/ewidget.cpp
@@ -1,5 +1,6 @@
 #include <lib/gui/ewidget.h>
 #include <lib/gui/ewidgetdesktop.h>
+#include <lib/gui/ewidgetanimation.h>
 
 extern void dumpRegion(const gRegion &region);
 
@@ -351,6 +352,28 @@ void eWidget::recalcClipRegionsWhenVisible()
 void eWidget::parentRemoved()
 {
 	m_parent = 0;
+}
+
+void eWidgetAnimation::startMoveAnimation(ePoint start, ePoint end, int length)
+{
+	m_move_current_tick = 0;
+	m_move_current_nexttick = 0;
+	m_move_length = length;
+	m_move_start = start;
+	m_move_end = end;
+	m_active = 1;
+	m_widget->move(m_move_start);
+}
+
+void eWidgetAnimation::stopMoveAnimation(ePoint start, ePoint end, int length)
+{
+	m_move_current_tick = 0;
+	m_move_current_nexttick = 0;
+	m_move_length = length;
+	m_move_start = start;
+	m_move_end = end;
+	m_active = 1;
+	m_widget->move(m_move_start);
 }
 
 int eWidget::event(int event, void *data, void *data2)
