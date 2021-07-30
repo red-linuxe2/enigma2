68afa99002ea7b2bf14ed8de8133c3927b737602
red-linuxe2 <88103659+red-linuxe2@users.noreply.github.com>
2021-07-30 15:59:43 +0100

    Update ewidget.cpp

--- lib/gui/ewidget.cpp
+++ lib/gui/ewidget.cpp
@@ -1,6 +1,8 @@
 #include <lib/gui/ewidget.h>
 #include <lib/gui/ewidgetdesktop.h>
 #include <lib/gui/ewidgetanimation.h>
+#include <lib/gdi/esize.h>
+#include <lib/gdi/epoint.h>
 
 extern void dumpRegion(const gRegion &region);
 
@@ -13,6 +15,8 @@ eWidget::eWidget(eWidget *parent): m_animation(this), m_parent(parent ? parent->
 	m_z_position = 0;
 	m_lowered = 0;
 	m_client_offset = eSize(0, 0);
+	m_move_current_tick = m_move_length;
+	m_move_current_nexttick = m_move_length;
 	if (m_parent)
 		m_vis = wVisShow;
 	if (m_parent)
